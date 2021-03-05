858dfd05d08dd2104657047077346cb8f9dda349
RGNGIT <68756004+RGNGIT@users.noreply.github.com>
2021-03-06 02:46:12 +0500

    Add files via upload

---
+++ _OOP_Lab1_1/_OOP_Lab1_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OOP_Lab1_1
+{
+    class Program
+    {
+
+        static int[] iArray;
+        static string sAction;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку формата <Число> +, -, *, / <Число>");
+            while (true)
+            {
+                vDivide(Console.ReadLine());
+                Console.WriteLine($"Результат: {vCalc()}");
+            }
+        }
+
+        static void vDivide(string Get)
+        {
+            string sTemp = string.Empty;
+            sAction = string.Empty;
+            iArray = new int[2];
+            for(int i = 0; i < Get.Length; i++)
+            {
+                if(Char.IsDigit(Get[i]))
+                {
+                    sTemp += Get[i].ToString();
+                }
+                else if(Get[i] != ' ')
+                {
+                    sAction = Get[i].ToString();
+                    iArray[0] = Convert.ToInt32(sTemp);
+                    sTemp = string.Empty;
+                }
+            }
+            iArray[1] = Convert.ToInt32(sTemp);
+        }
+
+        static int vCalc()
+        {
+            switch(sAction)
+            {
+                case "+":
+                    return iArray[0] + iArray[1];
+                case "-":
+                    return iArray[0] - iArray[1];
+                case "*":
+                    return iArray[0] * iArray[1];
+                case "/":
+                    return iArray[0] / iArray[1];
+                default:
+                    return 0;
+            }
+        }
+
+    }
+}
---
+++ _OOP_Lab2/_OOP_Lab2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OOP_Lab2
+{
+
+    class  Employee
+    {
+        private string sName;
+        private double fWage;
+
+        public Employee vCreateEmp()
+        {
+            return new Employee();
+        }
+
+        public void vRelease()
+        {
+            Console.WriteLine($"Имя: {this.sName}");
+            Console.WriteLine($"Зп: {this.fWage}");
+        }
+
+        public string sEmpName
+        {
+            get
+            {
+                return this.sName;
+            }
+            set
+            {
+                this.sName = value is string ? value : null;
+            }
+        }
+
+        public double fEmpWage
+        {
+            get
+            {
+                return this.fWage;
+            }
+            set
+            {
+                this.fWage = value;
+            }
+        }
+
+    }
+
+    class Program
+    {
+
+        static List<Employee> lEmp;
+        static Employee pEmp;
+
+        static void Main()
+        {
+            lEmp = new List<Employee>();
+            pEmp = new Employee();
+            for(int i = 0; i < 3; i++)
+            {
+                lEmp.Add(pEmp.vCreateEmp());
+            }
+            foreach(Employee i in lEmp)
+            {
+                i.sEmpName = Console.ReadLine();
+                i.fEmpWage = Convert.ToDouble(Console.ReadLine());
+            }
+            foreach (Employee i in lEmp)
+            {
+                i.vRelease();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
