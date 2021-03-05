557239f28a66320076cd84b318a8f6b8324d0c56
RGNGIT <68756004+RGNGIT@users.noreply.github.com>
2021-03-06 02:47:48 +0500

    Add files via upload

---
+++ _OOP_Matrix/_OOP_Matrix/CMatrix.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace _OOP_Matrix
+{
+    partial class CMatrix
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxRow1 = new System.Windows.Forms.TextBox();
+            this.textBoxColumns1 = new System.Windows.Forms.TextBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.buttonFill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxRow1
+            // 
+            this.textBoxRow1.Location = new System.Drawing.Point(573, 518);
+            this.textBoxRow1.Name = "textBoxRow1";
+            this.textBoxRow1.Size = new System.Drawing.Size(50, 20);
+            this.textBoxRow1.TabIndex = 1;
+            // 
+            // textBoxColumns1
+            // 
+            this.textBoxColumns1.Location = new System.Drawing.Point(629, 518);
+            this.textBoxColumns1.Name = "textBoxColumns1";
+            this.textBoxColumns1.Size = new System.Drawing.Size(50, 20);
+            this.textBoxColumns1.TabIndex = 2;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(686, 518);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 20);
+            this.buttonCreate.TabIndex = 3;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(687, 518);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(74, 20);
+            this.buttonCalc.TabIndex = 6;
+            this.buttonCalc.Text = "Вычислить";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // buttonFill
+            // 
+            this.buttonFill.Location = new System.Drawing.Point(687, 518);
+            this.buttonFill.Name = "buttonFill";
+            this.buttonFill.Size = new System.Drawing.Size(74, 20);
+            this.buttonFill.TabIndex = 7;
+            this.buttonFill.Text = "Заполнить";
+            this.buttonFill.UseVisualStyleBackColor = true;
+            this.buttonFill.Click += new System.EventHandler(this.buttonFill_Click);
+            // 
+            // CMatrix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 544);
+            this.Controls.Add(this.buttonFill);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.textBoxColumns1);
+            this.Controls.Add(this.textBoxRow1);
+            this.Name = "CMatrix";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxRow1;
+        private System.Windows.Forms.TextBox textBoxColumns1;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Button buttonFill;
+    }
+}
+
---
+++ _OOP_Matrix/_OOP_Matrix/CMatrix.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace _OOP_Matrix
+{
+
+    public partial class CMatrix : Form
+    {
+
+        public CMatrix()
+        {
+            InitializeComponent();
+            LButtons = new List<Button>();
+            LButtons.Add(buttonCreate);
+            LButtons.Add(buttonFill);
+            LButtons.Add(buttonCalc);
+            foreach(Button button in LButtons)
+            {
+                button.Visible = false;
+            }
+            vSetButtons(0, false);
+        }
+
+        bool bMtr = true;
+        List<Button> LButtons;
+        float[,] fMtr1, fMtr2;
+
+        // Позиция нулевого элемента 12;12 / Размер элемента 25;20 / Вбок 31 / Вниз 26
+
+        void vSwap(ref int iu, ref int iv)
+        {
+            int iTemp = iu;
+            iu = iv;
+            iv = iTemp;
+        }
+
+        void vCreateMatrix(bool bIsFirst)
+        {
+            int iColumns = iGetColumns();
+            int iRows = iGetRows();
+            int iXPos = 12, iYPos = 12;
+            if (!bIsFirst)
+            {
+                vSwap(ref iColumns, ref iRows);
+            }
+            for (int i = 0; i < iColumns; i++)
+            {
+                for (int j = 0; j < iRows; j++)
+                {
+                    TextBox CNewBox = new TextBox();
+                    CNewBox.Size = new Size(25, 20);
+                    CNewBox.Name = $"index[{bIsFirst}][{i}][{j}]";
+                    CNewBox.Location = new Point(iXPos + (i * 31), iYPos + (j * 26));
+                    Controls.Add(CNewBox);
+                }
+            }
+        }
+
+        void vCleaner(bool bIsFirst)
+        {
+            int iColumns = iGetColumns();
+            int iRows = iGetRows();
+            if (!bIsFirst)
+            {
+                vSwap(ref iColumns, ref iRows);
+            } 
+            for (int i = 0; i < iColumns; i++)
+            {
+                for (int j = 0; j < iRows; j++)
+                {
+                    Controls[$"index[{bIsFirst}][{i}][{j}]"].Visible = false;
+                }
+            }
+        }
+
+        void vReleaseResult(int iResSize)
+        {
+            int iXPos = 12, iYPos = 12;
+            for (int i = 0; i < iResSize; i++)
+            {
+                for (int j = 0; j < iResSize; j++)
+                {
+                    TextBox CNewBox = new TextBox();
+                    CNewBox.Size = new Size(25, 20);
+                    CNewBox.Text = vMul(fMtr1, fMtr2)[i, j].ToString();
+                    CNewBox.Location = new Point(iXPos + (i * 31), iYPos + (j * 26));
+                    if (vMul(fMtr1, fMtr2)[i, j] != 0)
+                    {
+                        Controls.Add(CNewBox);
+                    }
+                }
+            }
+        }
+
+        float[,] vMul(float[,] fMtr1, float[,] fMtr2)
+        {
+            float[,] fResMtr = new float[iResSize(), iResSize()];
+            for (int i = 0; i < fMtr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < fMtr2.GetLength(1); j++)
+                {
+                    for (int k = 0; k < fMtr2.GetLength(0); k++)
+                    {
+                        fResMtr[i, j] += fMtr1[i, k] * fMtr2[k, j];
+                    }
+                }
+            }
+            return fResMtr;
+        }
+
+        void vFillFMtr()
+        {
+            fMtr1 = new float[iGetColumns(), iGetRows()];
+            for (int i = 0; i < iGetColumns(); i++)
+            {
+                for (int j = 0; j < iGetRows(); j++)
+                {
+                    fMtr1[i, j] = Convert.ToInt32(Controls[$"index[{true}][{i}][{j}]"].Text);
+                }
+            }
+        }
+
+        void vFillSMtr()
+        {
+            fMtr2 = new float[iGetRows(), iGetColumns()];
+            for (int i = 0; i < iGetRows(); i++)
+            {
+                for (int j = 0; j < iGetColumns(); j++)
+                {
+                    fMtr2[i, j] = Convert.ToInt32(Controls[$"index[{false}][{i}][{j}]"].Text);
+                }
+            }
+        }
+
+        void vSetButtons(int iID, bool bHideFields)
+        {
+            LButtons[iID].Visible = true;
+            if(bHideFields)
+            {
+                textBoxColumns1.Visible = false;
+                textBoxRow1.Visible = false;
+            }
+            for(int i = 0; i < 3; i++)
+            {
+                if(iID != i)
+                {
+                    LButtons[i].Visible = false;
+                }
+            }
+        }
+
+        int iGetColumns() => Convert.ToInt32(this.textBoxRow1.Text);
+        int iGetRows() => Convert.ToInt32(this.textBoxColumns1.Text);
+
+        int iResSize() 
+        {
+            if (iGetColumns() == iGetRows())
+            {
+                return iGetColumns();
+            }
+            else
+            {
+                return iGetColumns() > iGetRows() ? iGetRows() + 1 : iGetColumns() + 1;
+            }
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            vCreateMatrix(bMtr);
+            vSetButtons(1, true);
+        }
+
+        private void buttonFill_Click(object sender, EventArgs e)
+        {
+            vFillFMtr();
+            vCleaner(bMtr);
+            bMtr = false;
+            vCreateMatrix(bMtr);
+            vSetButtons(2, true);
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            vFillSMtr();
+            vCleaner(bMtr);
+            vReleaseResult(iResSize());
+        }
+
+    }
+}
